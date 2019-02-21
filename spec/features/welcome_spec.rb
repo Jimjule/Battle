1934a1da40182ebad80759a0f8894367df4dea06
James Ault <james.ault@johnlewis.co.uk>
2019-02-21 14:26:22 +0000

    Test Helpers Refactor

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,17 +1,11 @@
 feature 'enter names' do
   scenario 'players see their names on screen after submitting in the form' do
-    visit('/')
-    fill_in :name_one, with: 'Jimmy'
-    fill_in :name_two, with: 'Hoju'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Jimmy vs. Hoju'
   end
 
   scenario "player 1 can see player 2's hit points" do
-    visit('/')
-    fill_in :name_one, with: 'Jimmy'
-    fill_in :name_two, with: 'Hoju'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Jimmy: 60 HP' && 'Hoju: 60 HP'
   end
 end
